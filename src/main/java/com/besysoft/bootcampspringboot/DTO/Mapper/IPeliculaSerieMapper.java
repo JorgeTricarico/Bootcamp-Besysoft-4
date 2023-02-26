376f3524d7cc13a8f8b2f71d528d10eb7c4d4ed3
Jorge Tricarico <jorge.tricarico@gmail.com>
2023-02-26 15:11:19 -0300

    add mapper to list

--- src/main/java/com/besysoft/bootcampspringboot/DTO/Mapper/IPeliculaSerieMapper.java
+++ src/main/java/com/besysoft/bootcampspringboot/DTO/Mapper/IPeliculaSerieMapper.java
@@ -2,12 +2,14 @@ package com.besysoft.bootcampspringboot.DTO.Mapper;
 
 import com.besysoft.bootcampspringboot.DTO.Request.PeliculaSerieRequestDto;
 import com.besysoft.bootcampspringboot.DTO.Response.PeliculaSerieResponseDto;
+import com.besysoft.bootcampspringboot.DTO.Response.PersonajeNameResponseDto;
 import com.besysoft.bootcampspringboot.dominio.PeliculaSerie;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {IPersonajeNameResponseMapper.class}, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface IPeliculaSerieMapper {
 
     @Mappings({
--- src/main/java/com/besysoft/bootcampspringboot/DTO/Mapper/IPersonajeMapper.java
+++ src/main/java/com/besysoft/bootcampspringboot/DTO/Mapper/IPersonajeMapper.java
@@ -3,13 +3,14 @@ package com.besysoft.bootcampspringboot.DTO.Mapper;
 import com.besysoft.bootcampspringboot.DTO.Request.PersonajeRequestDto;
 import com.besysoft.bootcampspringboot.DTO.Response.PersonajeResponseDto;
 import com.besysoft.bootcampspringboot.dominio.Personaje;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
 import javax.validation.constraints.Min;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {IPeliculaTitleMapper.class}, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface IPersonajeMapper {
 
     @Mappings({
